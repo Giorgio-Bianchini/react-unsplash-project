92d51ef4494c3e3c5cbcc8fddfbe2216fccd6c13
Giorgio Bianchini <gio.whiteh@gmail.com>
2024-08-26 18:20:26 +0200

    final project

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,6 +1,28 @@
+import { useGlobalContext } from "./context";
+
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
   return (
-    <h2>SearchForm</h2>
-  )
-}
-export default SearchForm
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="form-input search-input"
+          type="text"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
+export default SearchForm;
